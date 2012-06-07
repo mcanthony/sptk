eb82525c4dc282cd862a7ec73993dea7be407c68
okdtmhr <okdtmhr>
2012-06-07 07:02:38 +0000

    add proto type declaration of dct function

--- src/include/SPTK.h
+++ src/include/SPTK.h
@@ -43,7 +43,7 @@
 /* ----------------------------------------------------------------- */
 
 /***********************************************************
-   $Id: SPTK.h,v 1.40 2012/06/06 23:04:38 okdtmhr Exp $ 
+   $Id: SPTK.h,v 1.41 2012/06/07 07:02:38 okdtmhr Exp $ 
    
    Speech Signal Processing Toolkit
    SPTK.h
@@ -158,6 +158,8 @@ void ic2ir(double *h, const int leng, double *c, const int nc);
 void c2sp(double *c, const int m, double *x, double *y, const int l);
 void clip(double *x, const int l, const double min, const double max,
           double *y);
+void dct (double *in, double *out, const int size, const int m,
+          const Boolean dftmode, const Boolean compmode);
 int dct_create_table_fft(const int nSize);
 int dct_create_table(const int nSize);
 int dct_based_on_fft(float *pReal, float *pImag, const float *pInReal, const float *pInImag);
